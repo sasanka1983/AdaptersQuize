ee3410432525fdfd5d31de7f08a4cbc97eb4ac97
sasanka1983 <39948966+sasanka1983@users.noreply.github.com>
2019-04-27 20:43:01 +0530

    Initial commit

---
+++ QuizWebApi/Controllers/UserQuestionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuizWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserQuestionController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetQuestion()
+        {
+            return new string[] { "Q1", "A1" };
+        }
+    }
+}
